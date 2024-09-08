24bf340da167766e201cdcd090fce2f44849f3f1
risxbh <rsrisabhsingh212@gmail.com>
2024-09-08 15:18:07 +0530

    updates

--- routes/subCategoryRoutes.js
+++ routes/subCategoryRoutes.js
@@ -1,11 +1,19 @@
-const express = require('express');
-const { create, getAllCategories, upload,update,remove } = require('../controllers/subCategoryController');
+const express = require("express");
+const {
+  create,
+  getAllCategories,
+  upload,
+  update,
+  remove,
+  getSubCategoryByCategory,
+} = require("../controllers/subCategoryController");
 
 const router = express.Router();
 
-router.post('/create', upload.single('img'), create);
-router.get('/records', getAllCategories);
-router.post('/update',upload.single('img'), update);
-router.post('/delete', remove);
+router.post("/create", upload.single("img"), create);
+router.get("/records", getAllCategories);
+router.post("/update", upload.single("img"), update);
+router.post("/delete", remove);
+router.get("/getByCategory", getSubCategoryByCategory);
 
 module.exports = router;
